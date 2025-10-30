a4b27e00a96fd769356789093ff0f0652fcd183c
Mohd Asan <104296229+ofcyouritachii@users.noreply.github.com>
2025-10-30 09:06:52 +0000

    Remove addition script and implement factorial calculation

--- main.sh
+++ main.sh
@@ -1,6 +1,17 @@
-echo "Enter first no: "
-read a 
-echo "Enter second no: "
-read b
-c=`expr $a + $b`
-echo "Sum is : $c"
+# echo "Enter first no: "
+# read a 
+# echo "Enter second no: "
+# read b
+# c=`expr $a + $b`
+# echo "Sum is : $c"
+
+echo "enter number: "
+read n
+i=$n
+while test
+$i -ne 1
+do
+  i=`expr $i - 1`
+  n=`expr $n \* $i`
+done
+echo "Factorial is : $n"
